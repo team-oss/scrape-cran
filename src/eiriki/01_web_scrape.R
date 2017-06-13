8daa7b89a7c44277f81d534f996a1b194542dbb6
eiriki <eiriki@vt.edu>
2017-06-13 15:00:44 +0000

    basic scrape from a page

---
+++ src/eiriki/01_web_scrape.R
@@ -0,0 +1,57 @@
+#this is my attempt at web scraping in R
+library(RCurl)
+library(XML)
+library(stringr)
+library(rvest)
+
+
+#calling to get the information from the website HTML from rvest
+SFLink <- read_html("https://sourceforge.net/projects/audacity/?source=directory")
+
+#Get the Title
+OSS <- SFLink %>%
+  html_node('div h1') %>%
+  html_text() %>%
+  str_trim()
+
+#Get the Average Rating
+avg_rat <-SFLink %>%
+  html_node('section a') %>%
+  html_text() %>%
+  str_trim()
+
+#Get the Description
+Desc <- SFLink %>%
+  html_node('div h2') %>%
+  html_text() %>%
+  str_trim()
+
+#Get the Last update
+last_update <- SFLink %>%
+  html_node('section time') %>%
+  html_text() %>%
+  str_trim()
+
+#Get number of Ratings
+num_rat <- SFLink %>%
+  html_node('#counts-sharing span') %>%
+  html_text() %>%
+  str_trim()
+
+#Get weekly downloads
+week_down <- SFLink %>%
+  html_node('#call-to-action-stats') %>%
+  html_text() %>%
+  str_trim()
+
+#Get the category
+category <- SFLink %>%
+  html_node('li:nth-child(3) span') %>%
+  html_text() %>%
+  str_trim()
+
+#Get the date registered
+date_registered <- SFLink %>%
+  html_node('#project-awards+ .project-info .content') %>%
+  html_text() %>%
+  str_trim()
