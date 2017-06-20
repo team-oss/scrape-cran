4cd569388d2f0528c1f84ef2f2d2b6594c0c51a0
eiriki <eiriki@vt.edu>
2017-06-20 15:38:15 +0000

    fixed bugs (Date registered, description). Added "Project Type" (is it enterprise or project). Fixed Categories.

--- src/eiriki/01_web_scrape.R
+++ src/eiriki/01_web_scrape.R
@@ -1,6 +1,6 @@
 #this is my attempt at web scraping in R
 library(RCurl)
-library(XML)
+library(xml2)
 library(stringr)
 library(rvest)
 
@@ -27,6 +27,13 @@ sf_scrape <- function(link){
     html_node('div h2') %>%
     html_text() %>%
     str_trim()
+  #looking in another place for descriptions if it returns "Screenshots" (not real description)
+  if(desc == 'Screenshots'){
+    desc <- SFLink %>%
+      html_node('#description') %>%
+      html_text() %>%
+      str_trim()
+    }
 
 
   #Get the Last update
@@ -47,28 +54,53 @@ sf_scrape <- function(link){
     html_text() %>%
     str_trim()
 
+  #Tell me that this page is NOT an enterprise page (if it is, it will change to a diff function: detect later)
+  is_enterpise <- "Project"
+
   #Get the categories
   category <- SFLink %>%
     html_nodes('#breadcrumbs li~ li+ li span') %>%
     html_text() %>%
     str_trim()
 
-  #Get the date registered
-  #date_registered <- SFLink %>%
-   # html_nodes('#project-additional-trove .content') %>%
-    #html_text() %>%
-    #str_trim()
+  #Get the date registered ONLY IF THIS PAGE IS NOT AN ENTERPRISE PAGE. Otherwise, return NA
+  if(desc != "Overview") #Enterprise pages will usually return "Overview" for description
+  {
+  #Get all "additional details" nodes
   date_registered <- SFLink %>%
-    html_node('#project-awards+ .project-info .content') %>%
+    html_nodes('#project-additional-trove .content') %>%
     html_text() %>%
     str_trim()
 
-  #match <- grep(pattern = '200', x = date_registered)
-  #date_registered <- date_registered[match]
+  #filter out for the date
+  match <- grep(pattern = '20', x = date_registered)
+  date_registered <- date_registered[match]
+  }
+  else{
+    date_registered <- NA
+  }
+
+  #Get the authors in "Brought to you by"
+  authors <- SFLink %>%
+    html_nodes('#maintainers span') %>%
+    html_text() %>%
+    str_trim() %>%
+    paste(collapse = ', ')
+
+  #6/20 the code below does not work. trying xpath stuff to no avail.
+  #social_media <- SFLink %>%
+    #xml_find_all('//*[@id="aggregateCount"]') %>%
+    #xml_text() %>%
+    #str_trim()
+  #User features: ease, features, design, support (ratings for reach)
+  #Intended Audience (ex: developers)
+  #Programming Language
+  #Language
 
 
   v = list('OSS Title' = oss, 'Average Rating' = avg_rat, 'Description' = desc, 'Last Update' = last_update,
-           'Number of Ratings' = num_rat, 'Weekly Downloads' = week_down, 'Category 1' = category[1],
-           'Category 2' = category[2], 'Category 3' = category[3], 'Date registered' = date_registered)
+           'Number of Ratings' = num_rat, 'Weekly Downloads' = week_down, 'Project Type' = is_enterpise,
+           'Category 1' = category[1],'Category 2' = category[2], 'Category 3' = category[3],
+           'Date registered' = date_registered, 'Authors' = authors)
   return(data.frame(v, stringsAsFactors = F))
 }
--- src/eiriki/03_web_scrape_enterprise.R
+++ src/eiriki/03_web_scrape_enterprise.R
@@ -1,7 +1,7 @@
 #this will handle the case that a project on Sourceforge is an enterprise, which we have to
 #scrape differently
 library(RCurl)
-library(XML)
+library(xml2)
 library(stringr)
 library(rvest)
 
@@ -55,9 +55,12 @@ enterprise_scrape <- function(link){
 
   #Get the category
   category <- SFLink %>%
-    html_nodes('#breadcrumbs span') %>%
+    html_nodes('#breadcrumbs li+ li span') %>%
     html_text() %>%
     str_trim()
+
+  #This function is only called if the project is an enterprise so disntinguish that it's an enterprise
+  is_enterpise <- "Enterprise"
 
   #Get the date registered
   date_registered <- SFLink %>%
@@ -65,9 +68,21 @@ enterprise_scrape <- function(link){
     html_text() %>%
     str_trim()
 
+  #authors is NA for enterprise, this will do nothing
+  authors <- NA
+  #6/20 the code below does not work. trying xpath stuff to no avail.
+  #social_media <- SFLink %>%
+  #xml_find_all('//*[@id="aggregateCount"]') %>%
+  #xml_text() %>%
+  #str_trim()
+  #User features: ease, features, design, support (ratings for reach)
+  #Intended Audience (ex: developers)
+  #Programming Language
+  #Language
+
   v = list('OSS Title' = oss, 'Average Rating' = avg_rat, 'Description' = desc, 'Last Update' = last_update,
-           'Number of Ratings' = num_rat, 'Weekly Downloads' = week_down, 'Category 1' = category[1],
-           'Category 2' = category[2], 'Category 3' = category[3],
-           'Date registered' = date_registered)
+           'Number of Ratings' = num_rat, 'Weekly Downloads' = week_down, 'Project Type' = is_enterpise,
+           'Category 1' = category[1], 'Category 2' = category[2], 'Category 3' = category[3],
+           'Date registered' = date_registered, 'Authors' = authors)
   return(data.frame(v, stringsAsFactors = F))
 }
