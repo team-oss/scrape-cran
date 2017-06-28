c502dff8a2319406fccc9ec0f234f9d5e3b130e2
ckelling <ckelling@vt.edu>
2017-06-28 13:00:19 +0000

    changing null condition

--- src/ckelling/ScrapingCode/depsy/contrib_scrape/contrib_scrape.R
+++ src/ckelling/ScrapingCode/depsy/contrib_scrape/contrib_scrape.R
@@ -12,7 +12,31 @@ load(file= '~/git/oss/data/oss/original/depsy/all_packages_cran.Rdata')
 #Creating list of all packages on Cran and Depsy
 depsy_packages <- all_packages[-error_vec,] #these are the 9,810 R packages on Depsy and cran
 
-source(file = "~/git/oss/src/ckelling/ScrapingCode/depsy/03_row_function.R")
+#source(file = "~/git/oss/src/ckelling/ScrapingCode/depsy/03_row_function.R")
+
+
+makecontribRow <- function(name){
+  #name <- depsy_packages[1,1]
+  #link <- "http://depsy.org/api/package/cran/A3"
+  url <- paste('http://depsy.org/api/package/cran/', name, sep='')
+  document <- jsonlite::fromJSON(txt=url)
+  df <- c('name','contrib')
+  new_df <- c()
+
+  oss_name <- document$name
+  contribs <- document$all_contribs$name
+
+  if(length(document$all_contribs$name) > 0){
+    for(i in 1:length(document$all_contribs$name)){
+      contrib_name <- contribs[i]
+      new_row <- c(oss_name, contrib_name)
+      new_df <- rbind(new_df, new_row)
+      colnames(new_df) <- df
+    }
+  }
+  return(new_df)
+}
+
 
 contrib_mat <- c()
 for(i in 1:nrow(depsy_packages)){
