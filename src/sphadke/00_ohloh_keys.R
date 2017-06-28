72e8c4adba339df83c9fc3cdaebdc7a7ab70371c
Sayali Phadke <sphadke@vt.edu>
2017-06-28 16:47:25 +0000

    intermediate push

---
+++ src/sphadke/00_ohloh_keys.R
@@ -0,0 +1,23 @@
+## File contains all API keys for OpenHub
+# Source it to
+
+# API keys
+# Sayali's key
+oh_key_sp <- "f4b26446fe7946dc11e35e1e34e99aa9c2362b4294ce5d9799913fb6edcb7487"
+
+# Sayali's other key
+oh_key_ssp <- "8d65198778b2c33c216cc560af86d3d730ac3a936ead6cc297212b5b13bc01ae"
+
+# Alex's key
+oh_key_old <- "d32768dd2ec65efd004d19a9f3c7262d7f30cd8959d9009ce4f9b8e7e19ff0ef&v=1"
+
+# Ben's key
+oh_key_bjs <- "ea13e69a9fe006292249cffce39e96a5781088724a61cda6dba72fd9e71ecc06"
+
+# Lata's key
+oh_key_lk <- "60ec34006651da0607103a505cc688a4bdbf09b076798f5a31a330b4ac20bb32"
+
+# Kyle's key
+oh_key_km <- "95fd35a60145093710ed0dee5c2e39d1db1e54fbf09d4093ac29e1e613899bd6"
+
+
