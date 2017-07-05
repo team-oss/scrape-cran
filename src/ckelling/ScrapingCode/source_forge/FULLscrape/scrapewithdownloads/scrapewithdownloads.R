e774e294b7b4d5e1a9fa2b05badb3c06e9210b7c
ckelling <ckelling@vt.edu>
2017-07-05 20:59:55 +0000

    fixing error vec

--- src/ckelling/ScrapingCode/source_forge/FULLscrape/scrapewithdownloads/scrapewithdownloads.R
+++ src/ckelling/ScrapingCode/source_forge/FULLscrape/scrapewithdownloads/scrapewithdownloads.R
@@ -37,12 +37,13 @@ error_vec <- c()
 #load("~/git/oss/data/oss/original/sourceforge/errors.Rdata")
 #error_vec <- as.vector(error_vec[1,])
 #error_vec <- (error_vec[1:400,])
+error_vec <- c(1294,1639)
 error_vec <- as.data.frame(error_vec)
 #error_vec <- as.vector(error_vec[,1])
-#error_vec[,1] <- as.character(error_vec[,1])
+error_vec[,1] <- as.character(error_vec[,1])
 class(error_vec)
 
-for(i in 1:225000){
+for(i in 2311:225000){
   #for(i in 213737:213739){
   #i=213739
   new_data <- try(sf_scrape_only3(master_list_2[i]))
