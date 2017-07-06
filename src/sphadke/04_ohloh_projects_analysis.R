2031b24ef697488cc5614cccdad15f260c2e0ad5
Sayali Phadke <sphadke@vt.edu>
2017-07-06 16:50:49 +0000

    After Maddie's key failure!

--- src/sphadke/04_ohloh_projects_analysis.R
+++ src/sphadke/04_ohloh_projects_analysis.R
@@ -19,8 +19,38 @@ set.seed(312)
 ####################
 #### R Setup
 ####################
-library(httr)
 library(plyr)
-library(rvest)
 library(stringr)
-library(XML)
+
+load("~/git/oss/data/oss/working/openhub/randomProjects/all_random_projects_table.RData")
+
+
+####################
+#### Data quality check
+####################
+
+num_empty_rows <- abc
+
+for(i in 1:ncol(incident_table)){
+  x <- incident_table[,i]
+
+  # Make all blanks NAs
+  x[x == ""] <- NA
+
+  name <- colnames(incident_table)[i]
+  class <- class(x)
+
+  # Proportion of missing values
+  miss <- round(sum(is.na(x))*100/nrow(incident_table), digits = 2)
+
+  # How many unique values to the variable?
+  vals <- length(unique(x))
+  # summary <- summary(x)
+  if(vals <= 10){
+    tab <- table(x)
+    print(tab)
+  }
+  print(c(name, class, miss, vals))
+}
+
+
