4f07b61f9830ad0a67e4d2b3e2b5633b04b2a9ca
Sayali Phadke <sphadke@vt.edu>
2017-07-10 00:25:38 +0000

    Still working

--- src/sphadke/01_ohloh_user_ids.R
+++ src/sphadke/01_ohloh_user_ids.R
@@ -54,74 +54,40 @@ api_q <- function(path, page_no, api_key){
 # We create IDs for users/accounts
 # They go into a path, which then feeds into the API call to then pull table
 
-oh_key <- oh_key_sp
+# Run it once per key on 1000 pages
+oh_key <- oh_key_ssp
 
 ##Accounts
 account_ids <- vector()
-for (i in 1:1000){
+for (i in 982:1981){
   get_orgs <- api_q("/accounts", paste("page=", i, sep = ""), oh_key)
   orgs <- content(get_orgs, as = "parsed")
   ids <- str_split((xml_nodes(orgs, 'html_url') %>% html_text()), "/", simplify = TRUE)[,5]
-  ids
+  print(i)
   account_ids <- c(account_ids, ids)
 }
 
-org_ids <- unique(org_ids)
+account_ids <- unique(account_ids)
 save(org_ids, file = "~/git/oss/data/oss/original/openhub/all_org_ids.R")
 load("~/git/oss/data/oss/original/openhub/all_org_ids.R")
 
 
-####################
-#### Pulling the table
-####################
 
-## Table 'organization': takes projects
-# Creating a path that can directly go into the API function
-org_paths <- paste("/", "orgs", "/", org_ids, sep = "")
 
-organization <- matrix(NA, length(org_paths), 16)
-colnames(organization) <- c("org_url_id", "org_name", "created_date", "type",
-                            "portfolio_projects_count", "portfolio_projects",
-                            "affiliators", "affiliators_committing_to_portfolio_projects",
-                            "affiliator_commits_to_portfolio_projects", "affiliators_commiting_projects",
-                            "outside_committers", "outside_committers_commits", "projects_having_outside_commits",
-                            "outside_projects", "outside_projects_commits", "affiliators_committing_to_outside_projects")
-
-# The info we need
-for(i in 1:nrow(organization)){
-  contents <- api_q(org_paths[i], "", oh_key)
-
-  if(status_code(contents) == 200){
-    info <- content(contents, as = "parsed")
-
-    organization[i,1] <- org_ids[i]
-    organization[i,2] <- xml_node(info, 'name') %>% html_text()
-    organization[i,3] <- xml_nodes(info, 'created_at') %>% html_text()
-    organization[i,4] <- xml_nodes(info, 'type') %>% html_text()
-
-    organization[i,5] <- xml_contents(xml_nodes(info, 'portfolio_projects'))[1] %>% html_text()
-    organization[i,6] <- xml_nodes(xml_nodes(info, 'portfolio_projects'), "name") %>% html_text() %>% paste(collapse = ';')
-
-    organization[i,7] <- xml_nodes(info, 'affiliators') %>% html_text()
-    organization[i,8] <- xml_nodes(info, 'affiliators_committing_to_portfolio_projects') %>% html_text()
-    organization[i,9] <- xml_nodes(info, 'affiliator_commits_to_portfolio_projects') %>% html_text()
-    organization[i,10] <- xml_nodes(info, 'affiliators_commiting_projects') %>% html_text()
-
-    organization[i,11] <- xml_nodes(info, 'outside_committers') %>% html_text()
-    organization[i,12] <- xml_nodes(info, 'outside_committers_commits') %>% html_text()
-    organization[i,13] <- xml_nodes(info, 'projects_having_outside_commits') %>% html_text()
-
-    organization[i,14] <- xml_nodes(info, 'outside_projects') %>% html_text()
-    organization[i,15] <- xml_nodes(info, 'outside_projects_commits') %>% html_text()
-    organization[i,16] <- xml_nodes(info, 'affiliators_committing_to_outside_projects') %>% html_text()
-  } else {
-    organization[i,1] <- org_ids[i]
-  }
+project_ids <- vector()
+j <-  #number of the first page being pulled on this call
+k <-  #number of the last page being pulled on this call
+for (i in j:k){
+  get_projects <- api_q("/projects", paste("page=", i, sep = ""), oh_key)
+  projects <- content(get_projects, as = "parsed")
+  ids <- str_split((xml_nodes(projects, 'html_url') %>% html_text()), "/", simplify = TRUE)[,5]
+  ids
+  project_ids <- c(project_ids, ids)
   print(i)
 }
 
-# Save the table
-#save(organization, file = "~/git/oss/output/openhub/sample_of_10/org_table.R")
-write.csv(as.data.frame(organization), file = "~/git/oss/data/oss/original/openhub/organizations/all_orgs_table.csv")
-check <- read.csv("~/git/oss/data/oss/original/openhub/organizations/all_orgs_table.csv")
+project_ids <- unique(project_ids)
+save(project_ids, file = "./data/oss/original/openhub/projects/all_project_ids_5.RData")
+load("./data/oss/original/openhub/projects/all_project_ids_.RData")
+head(project_ids)
 
