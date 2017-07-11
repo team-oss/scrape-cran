d40cf0a003153af602efa5bd2d5a3d84b7efb5a1
Sayali Phadke <sphadke@vt.edu>
2017-07-11 13:20:39 +0000

    Final org analysis for mid-term

--- src/sphadke/03_ohloh_orgs_analysis.R
+++ src/sphadke/03_ohloh_orgs_analysis.R
@@ -20,7 +20,8 @@ set.seed(312)
 #### R Setup
 ####################
 library(ggplot2)
-library(stringr)
+library(reshape2)
+library(sdalr)
 
 ## Data import and rename
 load("~/git/oss/data/oss/final/openhub/organizations/all_orgs_table_final.RData")
@@ -29,15 +30,15 @@ load("~/git/oss/data/oss/final/openhub/organizations/all_orgs_table_final.RData"
 ####################
 #### Let's create cool plots!
 ####################
-cols <- c("Commercial" = "#80ced6", "Government" = "#d5f4e6",
-          "Education" = "#fefbd8", "Non-Profit" = "#618685")
+# cols <- c("Commercial" = "#80ced6", "Government" = "#d5f4e6",
+#           "Education" = "#fefbd8", "Non-Profit" = "#618685")
 
 #colors <- c("blue", "red", "dodgerblue4", "green")
 
 ## The main orgs plot
-ggplot(data = organization, aes(type, fill = type)) +
+p0 <- ggplot(data = organization, aes(type, fill = type)) +
   geom_bar() +
-  scale_fill_manual(values = cols) +
+  # scale_fill_manual(values = cols) +
   theme_minimal() +
   theme(legend.position="none") +
   ggtitle(paste("Organizations on OpenHub: Total=698")) +
@@ -48,7 +49,6 @@ ggplot(data = organization, aes(type, fill = type)) +
 ## Affiliated contributors
 p1 <- ggplot(data = organization, aes(x = type, y = affiliators, fill = type)) +
   geom_bar(stat = "identity") +
-  #scale_fill_manual(values = cols) +
   theme_minimal() +
   theme(legend.position="none") +
   ggtitle(paste0("Affiliators: Total=", sum(organization$affiliators))) +
@@ -56,22 +56,20 @@ p1 <- ggplot(data = organization, aes(x = type, y = affiliators, fill = type)) +
   theme(plot.title = element_text(hjust = 0.5))
 
 
-## Outside projects
-p2 <- ggplot(data = organization, aes(x = type, y = outside_projects, fill = type)) +
-  geom_bar(stat = "identity") +
-  #scale_fill_manual(values = cols) +
-  theme_minimal() +
-  theme(legend.position="none") +
-  ggtitle(paste0("Outside projects committed to by affiliators: Total=",
-                 sum(organization$outside_projects))) +
-  labs(x = "Organization type", y = "Outside projects") +
-  theme(plot.title = element_text(hjust = 0.5))
+# ## Outside projects
+# p2 <- ggplot(data = organization, aes(x = type, y = outside_projects, fill = type)) +
+#   geom_bar(stat = "identity") +
+#   theme_minimal() +
+#   theme(legend.position="none") +
+#   ggtitle(paste0("Outside projects committed to by affiliators: Total=",
+#                  sum(organization$outside_projects))) +
+#   labs(x = "Organization type", y = "Outside projects") +
+#   theme(plot.title = element_text(hjust = 0.5))
 
 
 ## Portfolio projects
 p3 <- ggplot(data = organization, aes(x = type, y = portfolio_projects_count, fill = type)) +
   geom_bar(stat = "identity") +
-  #scale_fill_manual(values = cols) +
   theme_minimal() +
   theme(legend.position="none") +
   ggtitle(paste0("Portfolio projects: Total=",
@@ -80,32 +78,51 @@ p3 <- ggplot(data = organization, aes(x = type, y = portfolio_projects_count, fi
   theme(plot.title = element_text(hjust = 0.5))
 
 
-## Outside contributors
-p4 <- ggplot(data = organization, aes(x = type, y = outside_committers, fill = type)) +
-  geom_bar(stat = "identity") +
-  #scale_fill_manual(values = cols) +
+# ## Outside contributors
+# p4 <- ggplot(data = organization, aes(x = type, y = outside_committers, fill = type)) +
+#   geom_bar(stat = "identity") +
+#   theme_minimal() +
+#   theme(legend.position="none") +
+#   ggtitle(paste0("Outside committers committing to portfolio projects: Total=", sum(organization$outside_committers))) +
+#   labs(x = "Organization type", y = "Outside contributors") +
+#   theme(plot.title = element_text(hjust = 0.5))
+
+
+# All committers to portfolio projects
+committer_df <- melt(data = organization, id.vars = c('org_name','type'), measure.vars = c('affiliators_committing_to_portfolio_projects', 'outside_committers'))
+
+colnames(committer_df)[3] <- "ContributorType"
+levels(committer_df$ContributorType) <- c("Affiliated", "Outside")
+
+p5 <- ggplot(data = committer_df, aes(x = type, y = value, fill = ContributorType)) +
+  geom_bar(stat = "identity", position = "dodge") +
   theme_minimal() +
-  theme(legend.position="none") +
-  ggtitle(paste0("Outside committers committing to portfolio projects: Total=", sum(organization$outside_committers))) +
-  labs(x = "Organization type", y = "Outside contributors") +
+  ggtitle(paste0("Committers to portfolio projects")) +
+  labs(x = "Organization type", y = "Contributors") +
   theme(plot.title = element_text(hjust = 0.5))
 
 
-# Affiliated committers to portfolio projects
+# All projects for affiliators
+project_df <- melt(data = organization, id.vars = c('org_name','type'), measure.vars = c('affiliators_commiting_projects', 'outside_projects'))
 
+colnames(project_df)[3] <- "ProjectType"
+levels(project_df$ProjectType) <- c("Portfolio", "Outside")
 
-ggplot(df.long,aes(Block,value,fill=variable))+
-  geom_bar(stat="identity",position="dodge")
-
-
-p5 <- ggplot(data = organization, aes(x = type, y = outside_committers, fill = type)) +
-  geom_bar(stat = "identity") +
-  #scale_fill_manual(values = cols) +
+p6 <- ggplot(data = project_df, aes(x = type, y = value, fill = ProjectType)) +
+  geom_bar(stat = "identity", position = "dodge") +
   theme_minimal() +
-  theme(legend.position="none") +
-  ggtitle(paste0("Outside committers committing to portfolio projects: Total=", sum(organization$outside_committers))) +
-  labs(x = "Organization type", y = "Outside contributors") +
+  ggtitle(paste0("Project contributions by affiliators")) +
+  labs(x = "Organization type", y = "Projects") +
   theme(plot.title = element_text(hjust = 0.5))
 
 
-multiplot(p1, p2, p3, p4, cols=2)
+##
+# Plot it!!!
+##
+p0
+multiplot(p1, p6, p3, p5, cols=2)
+
+
+
+
+
