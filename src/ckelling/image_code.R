ed6e242799c8fe5a85e7f782816638ba2bd1f403
ckelling <ckelling@vt.edu>
2017-07-12 11:44:50 +0000

    creating all final images for presentation

--- src/ckelling/image_code.R
+++ src/ckelling/image_code.R
@@ -1,54 +1,54 @@
 
 
 #cat_bar.png
-ggplot(agg_dat1)+ geom_bar(aes(x=Category.1, y=freq,fill = Category.2), stat= "identity")+ theme(legend.position="none",axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Categories and Subcategories")
+# ggplot(agg_dat1)+ geom_bar(aes(x=Category.1, y=freq,fill = Category.2), stat= "identity")+ theme(legend.position="none",axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Categories and Subcategories")
 
 #date_reg_time.png
-ggplot(summary, aes(x=date, y=freq))+
-  geom_line(aes(group = 1,colour = substr(date,6,7))) +
-  #geom_point() +
-  ggtitle("Date Registered Over Time") +
-  labs(x = "Date", y = "Total Update") +
-  theme(plot.title = element_text(hjust = 0.5))+
-  scale_colour_discrete(name="Month")
+# ggplot(summary, aes(x=date, y=freq))+
+#   geom_line(aes(group = 1,colour = substr(date,6,7))) +
+#   #geom_point() +
+#   ggtitle("Date Registered Over Time") +
+#   labs(x = "Date", y = "Total Update") +
+#   theme(plot.title = element_text(hjust = 0.5))+
+#   scale_colour_discrete(name="Month")
 
-#full_graph.png
-network_graph(agg_dat)
-network_graph(agg_dat) %>%
-  export_svg %>% charToRaw %>% rsvg_png("~/git/oss/src/ckelling/images/full_graph.pdf")
-library("animation")
-ani.options(outdir = "~/git/oss/src/ckelling/images")
-im.convert("~/git/oss/src/ckelling/images/full_graph.pdf", output = "full_graph.png")
+# #full_graph.png
+# network_graph(agg_dat)
+# network_graph(agg_dat) %>%
+#   export_svg %>% charToRaw %>% rsvg_png("~/git/oss/src/ckelling/images/new_images/full_graph.pdf")
+# library("animation")
+# ani.options(outdir = "~/git/oss/src/ckelling/images")
+# im.convert("~/git/oss/src/ckelling/images/full_graph.pdf", output = "full_graph.png")
 
 
 #last_up_time.png
-ggplot(summary, aes(x=date, y=freq))+
-  geom_line(aes(group = 1,colour = substr(date,6,7))) +
-  #geom_point() +
-  ggtitle("Last Update Over Time") +
-  labs(x = "Date", y = "Total Update") +
-  theme(plot.title = element_text(hjust = 0.5))+
-  scale_colour_discrete(name="Month")
+# ggplot(summary, aes(x=date, y=freq))+
+#   geom_line(aes(group = 1,colour = substr(date,6,7))) +
+#   #geom_point() +
+#   ggtitle("Last Update Over Time") +
+#   labs(x = "Date", y = "Total Update") +
+#   theme(plot.title = element_text(hjust = 0.5))+
+#   scale_colour_discrete(name="Month")
 
 #profile_table.png
-library(gridExtra)
-png(filename="~/git/oss/src/ckelling/images/profile_table.png",
-    units="in",
-    width=10,
-    height=10,
-    #pointsize=20,
-    res=72
-)
-p<-tableGrob(profile_dat)
-grid.arrange(p)
-dev.off()
+# library(gridExtra)
+# png(filename="~/git/oss/src/ckelling/images/new_images/profile_table.png",
+#     units="in",
+#     width=10,
+#     height=10,
+#     #pointsize=20,
+#     res=72
+# )
+# p<-tableGrob(profile_dat)
+# grid.arrange(p)
+# dev.off()
 
 #se_graph.png
-network_graph(se_counts)
-network_graph(se_counts) %>%
-  export_svg %>% charToRaw %>% rsvg_png("~/git/oss/src/ckelling/images/se_graph.pdf")
-ani.options(outdir = "~/git/oss/src/ckelling/images")
-im.convert("~/git/oss/src/ckelling/images/se_graph.pdf", output = "se_graph.png")
+# network_graph(se_counts)
+# network_graph(se_counts) %>%
+#   export_svg %>% charToRaw %>% rsvg_png("~/git/oss/src/ckelling/images/new_images/se_graph.pdf")
+# ani.options(outdir = "~/git/oss/src/ckelling/images/new_images")
+# im.convert("~/git/oss/src/ckelling/images/new_images/se_graph.pdf", output = "se_graph.png")
 
 #wordcloud_common.png
 commonality.cloud(test, max.words = 400, random.order = FALSE, colors = brewer.pal(8, "Dark2"))
@@ -60,7 +60,7 @@ comparison.cloud(test_compar, random.order=FALSE, colors = c("indianred3","steel
 
 
 library(gridExtra)
-png(filename="~/git/oss/src/ckelling/images/top_cat.png",
+png(filename="~/git/oss/src/ckelling/images/new_images/top_cat.png",
     units="in",
     width=10,
     height=10,
@@ -70,3 +70,5 @@ png(filename="~/git/oss/src/ckelling/images/top_cat.png",
 p<-tableGrob(top_cat)
 grid.arrange(p)
 dev.off()
+
+View(head(fullcontrib_mat, n=10))
