b30c8f9d8908af057249cc0e73e76ba9f9c6986f
eiriki <eiriki@vt.edu>
2018-06-15 14:24:25 +0000

    pushed local branch

--- src/scrape/CRAN/01_cran_scrape.R
+++ src/scrape/CRAN/01_cran_scrape.R
@@ -5,6 +5,7 @@ library(stringr)
 url <- "https://cran.r-project.org/web/packages/available_packages_by_date.html"
 url <- read_html(url)
 
+#function for each package
 cran_scrape <- function(link){
   #read in link to package
   append_link <- paste0("https://cran.r-project.org/web/packages", link)
