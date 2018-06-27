5490d1dfc110775f5e33d9f76824a7cd51690f1d
Daniel Chen <Maveri2k@yahoo.com>
2018-06-27 22:09:09 +0000

    started code_gove redo before api discovery

--- src/scrape/code_gov/02-parse_agencies.R
+++ src/scrape/code_gov/02-parse_agencies.R
@@ -2,47 +2,28 @@ library(rvest)
 library(xml2)
 library(stringr)
 
-parse_short_name <- function(x) {
-  a <- rvest::html_attr(x, 'href', default = NA)
-  split <- str_split(a, '/')[[1]]
-  return(split[length(split)])
-}
-
-parse_long_name <- function(x) {
-  return(html_text(x, trim = TRUE))
-}
-
-parse_href <- function(x) {
-  rel_path <- rvest::html_attr(x, 'href', default = NA)
-  return(sprintf('%s/%s', 'https://code.gov', rel_path))
-}
-
-#parse_short_name(x)
-#parse_long_name (x)
-
-
 webpage <- read_html('data/oss/original/code_gov/01-explore_code.html')
 
-# source_char <- readRDS('data/oss/original/code_gov/01-explore_code.RDS')
 
-#html_nodes(webpage, '.sidebar-list a')
+# Parse short names (abbreviations) ----
 
-sidebar_agencies <- html_nodes(webpage, '.sidebar-list a')
+(short_names <- webpage %>%
+  str_extract_all('href.*#/explore-code/agencies/.*?>') %>%
+  .[[1]] %>%
+  str_extract_all('(?<=#/explore-code/agencies/)(.*)(?=")') %>%
+  unlist() %>%
+  unique()
+)
 
-sidebar_agencies
+testthat::expect_equal(length(short_names), 25)
 
-short_names <- sapply(X = sidebar_agencies, FUN = parse_short_name)
-short_names
+# Make the urls from short names ----
 
-long_names <- sapply(X = sidebar_agencies, FUN = parse_long_name)
-long_names
+(urls <- paste0('https://code.gov/#/explore-code/agencies/', short_names))
 
-urls <- sapply(X = sidebar_agencies, FUN = parse_href)
-urls
+# Save and write out ----
 
 df <- data.frame('short_name' = short_names,
-                 'long_name' = long_names,
                  'url' = urls)
 
-saveRDS(df, 'data/oss/working/code_gov/agency_urls.RDS')
-write.csv(df, file = 'data/oss/working/code_gov/agency_urls.csv', )
+write.csv(df, file = 'data/oss/working/code_gov/agency_urls.csv', row.names = FALSE)
