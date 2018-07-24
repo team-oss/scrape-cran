e2d6ff4a61ddf770b99e03786ce28c1c6627ad14
Daniel Chen <Maveri2k@yahoo.com>
2018-07-24 17:35:40 +0000

    500 code_gov repos and its language, license, and gh info

---
+++ R/parse_github_slug.R
@@ -0,0 +1,22 @@
+#' takes in a github url, returns the org/repo slug
+#' @examples
+#' parse_github_slug('github.com/bi-sdal/sdalr')
+parse_github_slug <- function(github_url) {
+  if (is.na(github_url)) {
+    return(NA)
+  } else if (stringr::str_detect(github_url, 'github.com')) {
+    # regex for another time, let's not throw the kitchen sink when we don't have to
+    # stringr::str_extract(github_url, '(?<=github\\.com/)(.*?/.*?/?)')
+    after_github <- stringr::str_split_fixed(github_url, 'github.com/', 2)[, 2]
+    slug_components <- stringr::str_split_fixed(after_github, '/', 3)[, c(1, 2)]
+    return(paste(slug_components, collapse = '/'))
+  } else {
+    return(NA)
+  }
+}
+
+testthat::expect_equal(parse_github_slug('github.com/bi-sdal/sdalr'), 'bi-sdal/sdalr')
+testthat::expect_true(is.na(parse_github_slug('yahoo.com')))
+testthat::expect_true(is.na(parse_github_slug(NA)))
+testthat::expect_equal(parse_github_slug('https://github.com/usnationalarchives/AVI-MetaEdit/tree/master/Release'),
+                       'usnationalarchives/AVI-MetaEdit')
