294c5fe194301a2c909f630ed25281e9d0331df5
José Bayoán Santiago Calderón <jbsc@vt.edu>
2018-07-31 16:50:47 +0000

    julia_contributors_commits

--- src/scrape/Julia/contributors_and_commits_for_Julia.R
+++ src/scrape/Julia/contributors_and_commits_for_Julia.R
@@ -1,10 +1,23 @@
 pacman::p_load(sdalr, configr, dplyr, DBI, purrr, stringr, data.table, dtplyr,
                httr, jsonlite)
-github_token = '' # your Github API token
-# data = data.table() #  where data is the contributors data
-#until = today() # this should be max(data$end_date)
-# slugs = vector(mode = 'character') # this should be unique(data$slugs)
+github_token = ''
 headers = add_headers(Authorization = str_c('token ', github_token))
+
+read_julia = function() {
+  conn = con_db(dbname = 'oss',
+                pass = get_my_password())
+  output = dbReadTable(conn = conn,
+                       name = 'julia_contributions') %>%
+    data.table()
+  on.exit(expr = dbDisconnect(conn = conn))
+  return(value = output)
+}
+data = read_julia()
+until = max(data$end_date, na.rm = TRUE)
+slugs = data %>%
+  filter(!is.na(x = commits)) %>%
+  pull(var = slug) %>%
+  unique()
 
 github_commits_contributors = function(slug, until) {
   response = str_c('https://api.github.com/repos/',
@@ -12,18 +25,18 @@ github_commits_contributors = function(slug, until) {
                    '/commits?until=',
                    until) %>%
     GET(headers)
-  url = str_extract(string = response$headers$link,
-                    pattern = str_c('(?<=, <).*(?=>; rel="last")'))
-  base = str_extract(string = url,
-                     pattern = '\\d+$') %>%
-    as.integer()
-  if (base %in% 1L) {
+  if (is_null(x = response$headers$link)) {
     commits = response %>%
       content(as = 'text',
               encoding = 'UTF-8') %>%
       fromJSON() %>%
       nrow()
   } else {
+    url = str_extract(string = response$headers$link,
+                      pattern = str_c('(?<=, <).*(?=>; rel="last")'))
+    base = str_extract(string = url,
+                       pattern = '\\d+$') %>%
+      as.integer()
     base = (base - 1L) * 30L
     response = GET(url = url, headers)
     commits = response %>%
@@ -37,18 +50,18 @@ github_commits_contributors = function(slug, until) {
                    '/contributors?anon=false&until=',
                    until) %>%
     GET(headers)
-  url = str_extract(string = response$headers$link,
-                    pattern = str_c('(?<=, <).*(?=>; rel="last")'))
-  base = str_extract(string = url,
-                     pattern = '\\d+$') %>%
-    as.integer()
-  if (base %in% 1L) {
+  if (is_null(x = response$headers$link)) {
     contributors = response %>%
       content(as = 'text',
               encoding = 'UTF-8') %>%
       fromJSON() %>%
       nrow()
   } else {
+    url = str_extract(string = response$headers$link,
+                      pattern = str_c('(?<=, <).*(?=>; rel="last")'))
+    base = str_extract(string = url,
+                       pattern = '\\d+$') %>%
+      as.integer()
     base = (base - 1L) * 30L
     response = GET(url = url, headers)
     contributors = response %>%
@@ -60,9 +73,27 @@ github_commits_contributors = function(slug, until) {
   output = data.table(slug = slug,
                       until = until,
                       commits = commits,
-                      contributors)
+                      contributors = contributors)
+  if (as.integer(x = response$headers$`x-ratelimit-remaining`) < 5L) {
+    Sys.sleep(time = 3.6e3)
+  }
   return(value = output)
-}
+  }
 
-output = map_df(.x = slugs,
-                .f = github_commits_contributors)
+output = map2_df(.x = slugs,
+                 .y = until,
+                 .f = github_commits_contributors)
+
+upload_julia_contributors_commits = function(data) {
+  conn = con_db(dbname = 'oss',
+                pass = get_my_password())
+  output = dbWriteTable(conn = conn,
+                        name = 'julia_contributors_commits',
+                        value = data,
+                        row.names = FALSE,
+                        overwrite = TRUE) %>%
+    data.table()
+  on.exit(dbDisconnect(conn = conn))
+  return(value = output)
+  }
+upload_julia_contributors_commits(data = output)
