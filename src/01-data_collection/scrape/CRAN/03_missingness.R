21582b74734f72b05f6e6143b01c19f1f9b83b8d
Daniel Chen <dc4fj@virginia.edu>
2018-12-14 18:20:45 +0000

    save missing values to new vars instead of replacing

--- src/01-data_collection/scrape/CRAN/03_missingness.R
+++ src/01-data_collection/scrape/CRAN/03_missingness.R
@@ -6,6 +6,6 @@ data <- data.frame(data)
 na_count <-colSums(is.na(data))
 
 na_count <- data.frame(na_count)
-na_count <- na_count / 12614 #make percentage missing
-na_count <- round(na_count, digits =4) *100
+na_count$prop_missing <- na_count$na_count / nrow(data) # 12614 #make percentage missing
+na_count$pct_missing <- round(na_count$prop_missing, digits =4) *100
 na_count
