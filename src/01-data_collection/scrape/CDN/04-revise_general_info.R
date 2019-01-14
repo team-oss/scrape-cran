a089e9cb3c396aec91b1d616447f4e71d508883a
Daniel Chen <chend@virginia.edu>
2019-01-14 15:19:05 +0000

    keren changes

--- src/01-data_collection/scrape/CDN/04-revise_general_info.R
+++ src/01-data_collection/scrape/CDN/04-revise_general_info.R
@@ -1,21 +1,35 @@
+#parse the general info df again to improve data quality
+
+#libraries
 library(stringr)
 library(dplyr)
 library(rvest)
 library(jsonlite)
 library(data.table)
+source("functions_keren.R")
+
+#load general_info
 general_info <- read.csv("data/oss/final/CDN/general_info.csv",
                          stringsAsFactors = FALSE, check.names=FALSE)
 
+#add a new column denoting whether a repo is on github
+#this column is mainly useful for 06 and final graph drawing
+#this feature is added late
+#however, 02 involves detecting if a repo is on github
+#thus, next step we can revise general_info first and abbreviate some code in 02
 general_info$github <- str_detect(general_info$repository.url, "github")
-###########################################################################
-fix_url = function(repo) {
-  fixed = str_replace(repo, "^.*(?<=github.com.)", "https://github.com/") %>%
-    str_remove("(/|\\.git)$")
-  return(fixed)
-}
-#str(general_info)
+
+
+
+#functions
+
+#get all languages involved in a pakcage using github API
+#github is a boolean denoting whether this repo is on github
+#repo is a repo url
+#returns a vector of the percentage of all languages involved
 check_lang = function(github, repo) {
   if (!is.na(github) & github) {
+    #request language percentage report
     response = str_c('https://api.github.com/',
                      'repos',
                      '/',
@@ -27,6 +41,7 @@ check_lang = function(github, repo) {
       GET(add_headers(Authorization = 'token 2d260070668afe675673e973faf2ec30b48e831c'))
     Sys.sleep(time = 5e-1)
 
+    #if the report is avalible, download and collapse it
     if (status_code(response) == 200) {
       output = response %>%
         content(as = 'text', encoding = 'UTF-8') %>%
@@ -35,8 +50,14 @@ check_lang = function(github, repo) {
       return(unlist(output))
     }
   }
+  #otherwise, lang percentage is 0
   return(as.integer(0))
 }
+
+#parse the result from check_lang
+#it reshapes the result into a df with col 1 as languages and col2 as percentages
+#langs is a result from check_lang
+#returns the resulting df
 lang_percentage = function(langs) {
   total = sum(langs)
   if (total != 0L) {
@@ -46,6 +67,11 @@ lang_percentage = function(langs) {
     return(data.frame())
   }
 }
+
+#identify a package's language with highest probability
+#based on lang_percentage result
+#name is a package's name
+#returns the package's main language
 prob_lang = function (name) {
   percentage = lang_df$percentage[lang_df$name == name][1]
   if (!is.na(percentage) & percentage >= 50) {
@@ -53,7 +79,13 @@ prob_lang = function (name) {
   }
   return("Other")
 }
+
+#further identify and parse the main language of a package
+#row is a composite df
+#with col 1 as pkg name, col 2 as description, col 3 as highest percenage lang
+#returns deducted main lang
 confirm_lang = function(row) {
+  #first infer from name
   if (str_detect(row[1], regex("javascript", TRUE)) |
       str_detect(row[1], regex("js", TRUE)) |
       str_detect(row[1], regex("angular", TRUE))) {
@@ -64,6 +96,7 @@ confirm_lang = function(row) {
     return("HTML")
   }
 
+  #if not infered from name, infer from description
   if (str_detect(row[2], regex("javascript", TRUE)) |
       str_detect(row[2], regex("js", TRUE))) {
     return("JavaScript")
@@ -73,10 +106,15 @@ confirm_lang = function(row) {
     return("html")
   }
 
+  #return lang with highest percentage
     return(row[3])
 }
 ###########################################################################
+#script
+
+#add a column of trimmed url
 general_info$repo <- lapply(general_info$repository.url, fix_url) %>% unlist()
+
 #manually fix a case where reop is switched to github
 general_info$repo[776] = "https://github.com/datejs/Datejs"
 general_info$github[776] = TRUE
@@ -94,9 +132,7 @@ general_info$repo[2270] = "https://github.com/wikimedia/oojs"
 general_info$github[2270] = TRUE
 general_info$repo[3073] = "https://gitlab.com/epistemex/transformation-matrix-js"
 
-
-# repos = general_info$repo[general_info$github %in% TRUE]
-# repos = repos[1:10]
+#use check_lang to obtain download coarse language reports
 lang_results = list()
 for(i in 1:length(general_info$repo)) {
   components = check_lang(general_info$github[], general_info$repo[i]) %>% lang_percentage()
@@ -110,23 +146,28 @@ for(i in 1:length(general_info$repo)) {
   print(i)
 }
 
+#save lang reports
 lang_df = do.call(what = rbind, args = lang_results)
 write.csv(lang_df, file = "data/oss/working/CDN/pkg_langs.csv", row.names=FALSE)
-write.csv(lang_df, file = "data/oss/final/CDN/general_info.csv", row.names=FALSE)
 
-# if only the language with most percentage is needed, lang_df is not useful; do all the operation with lang_results
+#detenct languages with most percentages
 dominant_pkg_lang = lapply(general_info$name, prob_lang)
 dominant_pkg_lang = do.call(what = rbind, args = dominant_pkg_lang)
+#final inference of package lang
 temp = data.frame(name = general_info$name, description = general_info$description, percentage_lang = dominant_pkg_lang,
                   stringsAsFactors = FALSE)
 temp1 = lapply(temp, confirm_lang)
 
+#slugs are in the form of repOwner/repo, languages are pkg langs
 general_info$slugs = str_extract(general_info$repo, "(?<=github\\.com(/|:)).*")
 general_info$language = temp1$selected_language
 
+
+#save general_info and final infered lang results
 write.csv(temp1, file = "data/oss/working/CDN/pkg_langs_finalized.csv", row.names=FALSE)
 write.csv(general_info, file = "data/oss/final/CDN/general_info.csv", row.names=FALSE)
 
+#upload result
 library(DBI)
 library(sdalr)
 my_db_con <- con_db("oss", pass=sdalr::get_my_password())
@@ -135,6 +176,6 @@ dbWriteTable(con = my_db_con,
              value = general_info[c("name", "slugs", "language", "description", "github", "repo")],
              row.names = FALSE,
              overwrite = TRUE)
-##############################
-lang_df <- read.csv("data/oss/working/CDN/pkg_langs.csv",
-                         stringsAsFactors = FALSE, check.names=FALSE)
+
+
+on.exit(dbDisconnect(conn = xxx))
