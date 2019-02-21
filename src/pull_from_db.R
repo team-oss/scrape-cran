952056706f5ef24cb50f5636d4c878c41d91f184
Aaron D. Schroeder <aschroed@virginia.edu>
2019-02-21 19:12:34 +0000

    db combinations

--- src/pull_from_db.R
+++ src/pull_from_db.R
@@ -5,6 +5,7 @@ library(DBI)
 # Create db connection
 con <- con_db(dbname = "oss", host = "127.0.0.1", port = 5433, user = "aschroed", pass = "Iwnftp$2")
 
+
 # Get top 100 packages for a given country and year
 pkg_dnld_2018 <- dbGetQuery(con, "SELECT *
                        FROM cran_logs_pkg_year
@@ -13,4 +14,12 @@ pkg_dnld_2018 <- dbGetQuery(con, "SELECT *
                        ORDER BY pkg_cnt DESC
                        LIMIT 100")
 
-head(pkg_dnld_2018)
+head(pkg_dnld_2018, n=20)
+
+
+# Get R download total per country per year
+r_dwnlds_cntry_yr <- dbGetQuery(con, "SELECT *
+                       FROM cran_logs_r_year
+                       ORDER BY country, year, dwnld_cnt DESC")
+
+head(r_dwnlds_cntry_yr, n=100)
